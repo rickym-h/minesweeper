05e1cc29d6699d2a4f70607214ce7982495a63d6
rickym-h <richardhull11@gmail.com>
2022-07-04 20:57:52 +0100

    cleaned up code ready for minesweeper implementation

--- src/main/java/com/minesweeper/minesweeper/MyApplication.java
+++ src/main/java/com/minesweeper/minesweeper/MyApplication.java
@@ -1,20 +1,39 @@
 package com.minesweeper.minesweeper;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MyApplication extends Application {
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+
+        // Set the title of the window
+        primaryStage.setTitle("Ricky's Minesweeper Game");
+
+        // Create a new button which logs to console when clicked
+        Button button = new Button("Click Me");
+        button.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Clicked!");
+            }
+        });
+
+        // Add nodes to the root
+        root.getChildren().add(button);
+
+        // Create a new window using the root and all it's sub-nodes
+        Scene scene = new Scene(root, 480, 480);
+
+        // Set scene and show the window
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
